98ba1fe1f4d45ed9581f50fce47e286952ad5332
Karl Robert Kristenprun <kristenbrun@hotmail.com>
2023-03-15 20:53:34 +0100

    on -> onSet

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -49,13 +49,13 @@ export class HttpRemoteAccessory {
     // turn device on/off
     this.tvService
 			.getCharacteristic(Characteristic.Active)
-			.on('set', this.setActive.bind(this))
+			.onSet(this.setActive.bind(this))
 			.on('get', this.getActive.bind(this));
 
     // set sources, 
     this.tvService
 			.getCharacteristic(Characteristic.ActiveIdentifier)
-			.on('set', this.setActiveIdentifier.bind(this))
+			.onSet(this.setActiveIdentifier.bind(this))
 			.on('get', this.getActiveIdentifier.bind(this));
 
     // remote control input
@@ -64,62 +64,62 @@ export class HttpRemoteAccessory {
     .onSet(this.setButton.bind(this));
   }
 
-  async setButton(
-    value: CharacteristicValue,
-  ) {
-    this.platform.log.debug('Set Characteristic Active ->', value);
-    try {
-      let url = "";
-    
-      switch(value) {
-        case 0: {
-          break;
-        }
-        case 1: {
-          break;
-        }
-        case 2: {
-          break;
-        }
-        case 3: {
-          break;
-        }
-        case 4: {
-          url = this.accessory.context.device.button?.upUrl;
-          break;
-        }
-        case 5: {
-          url = this.accessory.context.device.button?.downUrl;
-          break;
-        }
-        case 6: {
-          url = this.accessory.context.device.button?.leftUrl;
-          break;
-        }
-        case 7: {
-          url = this.accessory.context.device.button?.rightUrl;
-          break;
-        }
-        case 8: {
-          url = this.accessory.context.device.button?.selectUrl;
-          break;
-        }
-        case 9: {
-          url = this.accessory.context.device.button?.backUrl;
-          break;
-        }
-        case 10: {
-          break;
-        }
-        case 11: {
-          break;
-        }
-        case 15: {
-          url = this.accessory.context.device.button?.infoUrl;
-          break;
-        }
-      }
-      await this.httpRequest(
+	async setButton(
+	value: CharacteristicValue,
+	) {
+	this.platform.log.debug('Set Characteristic Active ->', value);
+	try {
+		let url = "";
+
+		switch(value) {
+		case 0: {
+			break;
+		}
+		case 1: {
+			break;
+		}
+		case 2: {
+			break;
+		}
+		case 3: {
+			break;
+		}
+		case 4: {
+			url = this.accessory.context.device.button?.upUrl;
+			break;
+		}
+		case 5: {
+			url = this.accessory.context.device.button?.downUrl;
+			break;
+		}
+		case 6: {
+			url = this.accessory.context.device.button?.leftUrl;
+			break;
+		}
+		case 7: {
+			url = this.accessory.context.device.button?.rightUrl;
+			break;
+		}
+		case 8: {
+			url = this.accessory.context.device.button?.selectUrl;
+			break;
+		}
+		case 9: {
+			url = this.accessory.context.device.button?.backUrl;
+			break;
+		}
+		case 10: {
+			break;
+		}
+		case 11: {
+			break;
+		}
+		case 15: {
+			url = this.accessory.context.device.button?.infoUrl;
+			break;
+		}
+		}
+		await this.httpRequest(
 				url,
 				this.accessory.context.device.button?.method ||
 					this.accessory.context.device.method,
@@ -129,22 +129,21 @@ export class HttpRemoteAccessory {
 				},
 				this.accessory.context.device.button?.body
 			);
-      this.states.RemoteKey = value as number;
-    } catch (e) {
-      this.platform.log.error('Cannot update TV power via HTTP', e);
-    }
-  }
+		this.states.RemoteKey = value as number;
+	} catch (e) {
+		this.platform.log.error('Cannot update TV power via HTTP', e);
+	}
+	}
 
 	async setActive(
-		value: CharacteristicValue,
-		callback: CharacteristicSetCallback
+		value: CharacteristicValue
 	) {
 		this.platform.log.debug('Set Characteristic Active ->', value);
 		try {
 			const url = (value as boolean)
 				? this.accessory.context.device.power?.onUrl
 				: this.accessory.context.device.power?.offUrl;
-    
+
 			if (!url) {
 				throw new Error(`TV ${this.accessory.context.device.name} does not have an ${(value as boolean) ? 'on' : 'off'}Url`);
 			}
@@ -161,10 +160,8 @@ export class HttpRemoteAccessory {
 			);
 
 			this.states.Active = value as boolean;
-			callback(null);
 		} catch (e) {
 			this.platform.log.error('Cannot update TV power via HTTP', e);
-			callback(e);
 		}
 	}
 
@@ -176,8 +173,7 @@ export class HttpRemoteAccessory {
 	}
 
 	async setActiveIdentifier(
-		value: CharacteristicValue,
-		callback: CharacteristicSetCallback
+		value: CharacteristicValue
 	) {
 		this.platform.log.debug(
 			'Set Characteristic ActiveIdentifier -> ',
@@ -204,10 +200,8 @@ export class HttpRemoteAccessory {
 			);
 
 			this.states.ActiveIdentifier = value as number;
-			callback(null);
 		} catch (e) {
 			this.platform.log.error('Cannot update input change via HTTP', e);
-			callback(e);
 		}
 	}
 
